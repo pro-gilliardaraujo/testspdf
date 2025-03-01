0f27c4ea831923f979430c18ff810785624b67a2
Desenvolvimento <desenvolvimento.iblogistica@gmail.com>
2025-02-28 22:23:47 -0300

    feat: ajustes de configuração do servidor e CORS

--- src/config/cors.js
+++ src/config/cors.js
@@ -1,9 +1,12 @@
 const corsOptions = {
     origin: '*', // Em produção, você deve especificar os domínios permitidos
-    methods: ['GET', 'POST', 'PUT', 'DELETE', 'OPTIONS'],
-    allowedHeaders: ['Content-Type', 'Authorization'],
+    methods: ['GET', 'POST', 'PUT', 'DELETE', 'OPTIONS', 'PATCH'],
+    allowedHeaders: ['Content-Type', 'Authorization', 'X-Requested-With', 'Accept', 'Origin'],
+    exposedHeaders: ['Content-Range', 'X-Content-Range'],
     credentials: true,
-    optionsSuccessStatus: 200
+    preflightContinue: false,
+    optionsSuccessStatus: 200,
+    maxAge: 3600
 };
 
 module.exports = corsOptions; 
